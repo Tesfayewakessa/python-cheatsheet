da253acae9679bd06fe72757a341869ec913ad2c
Tesfaye <tesfayewakessa@gmail.com>
2022-08-11 02:51:25 +0800

    numbers.py added. A cheatsheet for various arithmetic operations

---
+++ numbers.py
@@ -0,0 +1,29 @@
+#using python as a calculator
+
+'''
+Arithmetic operators:
+
+we have a whool bunch of operators at our disposal
+ + Addition
+ - Subtruction
+ * Multiplication
+ / Division
+ // Floor division
+ % Modulus (remainder of x / y ) or use divmod(x,y)
+ ** Exponentian (power of ) | can also use pow(x,y) instead of x**y
+
+ Asignment operators:
+ = Equals
+
+ Number types:
+ int 
+ float
+
+ Built-in function:
+
+ round()  ---> rounds numbers to the specified number of decimals i.e round(number, decimals)
+ 
+
+ Finally, python has a hamdy of handling big int's easier to read
+ 4000000000 can be read as 4_000_000_000
+'''
