8a4e89340a5f86f92c1d4978ad272dc1736e3c14
Tesfaye <tesfayewakessa@gmail.com>
2022-08-13 08:38:02 +0800

    Modified numbers.py and added strings.py

--- numbers.py
+++ numbers.py
@@ -11,13 +11,14 @@ we have a whool bunch of operators at our disposal
  // Floor division
  % Modulus (remainder of x / y ) or use divmod(x,y)
  ** Exponentian (power of ) | can also use pow(x,y) instead of x**y
-
+bodmas
+(/*+-)
  Asignment operators:
  = Equals
 
  Number types:
- int 
- float
+ int (1,2,3,4,5)
+ float (1.2,2.3,3.4,4.567)
 
  Built-in function:
 
@@ -29,6 +30,9 @@ we have a whool bunch of operators at our disposal
  4000000000 can be read as 4_000_000_000
 '''
 # The basics 
+from turtle import width
+
+
 2 + 2 #simple addition
 2 - 2 # simple subtruction
 2*2 #simple multiplication
@@ -43,3 +47,26 @@ round(10.0234,2)
 2**4 # exponentiation
 pow(2,4)
 assert(2**4 == pow(2,4))
+
+#BODMAS
+2-3*4 #=2-(3*4)=-10
+(20-3*4)/4 # =(20-(3*4))/4 = 2
+9-16/2*4+6-4/4 # = 9-(16/2)*4+6-(4/4) = -18------> brackets of division---->multiplication------->additon------>subtruction
+
+#variable assignments
+
+width = 20
+height = 5
+length = 20
+
+area = width*height
+volume = length*area
+
+# interactive mode, the last printed expression is assigned to the variable _
+tax = 12.5/100
+price = 100.25
+
+price*tax # the result of the multiplication is assigned to a variable '_'
+
+price + _ # we reference _ but the expression itself is assigned to _
+round(_,2) 
